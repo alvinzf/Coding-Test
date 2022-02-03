90d098d153839c738c93fb2cb4fe7a33b076ff40
Alvin Zulham <alvinzulhamm@gmail.com>
2022-02-03 17:48:43 +0700

    add : comments

--- 1.js
+++ 1.js
@@ -1,20 +1,24 @@
 function findMinMax(arr, m) {
+  //get array length
   let n = arr.length;
+  //define variables for sum
   let min_sum = 0;
   let max_sum = 0;
 
+  //sort array
   arr.sort(function (a, b) {
     return a - b;
   });
 
+  //loop sum array value
   for (let i = 0, j = n - 1; i < m; i++, j--) {
     min_sum += arr[i];
     max_sum += arr[j];
   }
-
+  //print value
   console.log("Min value: " + min_sum + " " + "Max Value: " + max_sum + " ");
 }
-
+//define array
 let arr_a = [1, 2, 3, 4, 5];
 let arr_b = [2, 4, 6, 8, 10];
 findMinMax(arr_a, 4);
