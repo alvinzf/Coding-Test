f7b648f795da8397dffe0bc8d1769bd078fd5dc7
Alvin Zulham <alvinzulhamm@gmail.com>
2022-02-05 08:26:32 +0700

    remove comment

--- 2.php
+++ 2.php
@@ -1,26 +1,21 @@
 <?php
-//start
+
 $n = 1;
 
-//end
 $m = 100;
-//loop for the function defined below
+
 for ($i = $n; $i <= $m; $i++) {
     sakumas($i);
 }
 
 function sakumas($num)
 {
-    //cek if the number can be divided by 3 and 7
+
     if ($num % 3 == 0 && $num % 7 == 0) {
         echo "sakumas, ";
-    }
-    //cek if the number can be divided by 3
-    elseif ($num % 3 == 0) {
+    } elseif ($num % 3 == 0) {
         echo "saku, ";
-    }
-    //cek if the number can be divided by 7
-    elseif ($num % 7 == 0) {
+    } elseif ($num % 7 == 0) {
         echo "mas, ";
     } else {
         echo $num . ", ";
